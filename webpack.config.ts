c8356e37c36f7bb4cf89348ee279b55b888680b8
g.valeev <g.valeev@xsolla.com>
2020-09-22 12:12:35 +0500

    feat: add webpack config

---
+++ webpack.config.ts
@@ -0,0 +1,82 @@
+import webpack, { Configuration } from 'webpack';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+const fileName = (ext: string) => (isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`);
+const jsLoaders = () => {
+  const loaders = ['babel-loader'];
+
+  if (isDev) {
+    loaders.push('eslint-loader');
+  }
+
+  return loaders;
+};
+
+const clientConfig: Configuration = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './index.ts',
+  output: {
+    filename: fileName('js'),
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    port: 4000,
+    hot: isDev,
+    contentBase: path.resolve(__dirname, 'dist'),
+    writeToDisk: true,
+  },
+  devtool: isDev ? 'source-map' : false,
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true,
+            },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(ts|tsx|js)\$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.scss'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core'),
+    },
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: fileName('css'),
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
+
+export default clientConfig;
